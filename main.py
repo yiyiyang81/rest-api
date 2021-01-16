462e140ed2a27f05edd91bee9598326c9efed215
Yiyi Yang <57012409+yiyiyang81@users.noreply.github.com>
2021-01-16 22:08:11 +0800

    Update main.py
    Parses arguments stored in the memory, sends status code for request validation.

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort, reqparse
 
 app = Flask(__name__)
 api = Api(app) #wrap app in the api(restful)
 
-class Grape(Resource):
-    def get(self):
-        return {"data":"Gilbert Grape"}
 
-api.add_resource(Grape, "/hellowworld")
+name_args = reqparse.RequestParser()
+name_args.add_argument("ID", type=str, help="ID of the employee", required = True)
+name_args.add_argument("name", type=str, help="Name of the employee", required=True)
+name_args.add_argument("position", type=str, help="Position of the employee")
+
+names = {}
+
+# "Leslie":{"name":"Leslie", "ID": "001", "position":"Co-Founder"}, 
+#         "Sarah":{"name":"Sarah", "ID": "002", "position": "CEO"}
+
+def error_invalid_employee(name):
+    if name not in names:
+        abort(404, message ="Employee not found")
+
+
+class HelloWorld(Resource):
+    def get(self, name):
+        error_invalid_employee(name)
+        return names[name]
+
+    def register(self, name):
+        args = name_args.parse_args()   #gets input elements from names
+        names[name] = args
+        return names[name], 201     #201 for succesful creation
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True) #debug mode
